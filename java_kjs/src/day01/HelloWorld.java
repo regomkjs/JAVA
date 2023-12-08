0ccc613955972c4685c9d2cd7ec2b224f05d6d3d
regomkjs <regomkjs2013@gmail.com>
2023-12-08 14:24:24 +0900

    자바 1일차 샘플 코드 및 설명

--- java_kjs/src/day01/HelloWorld.java
+++ java_kjs/src/day01/HelloWorld.java
@@ -1,9 +1,15 @@
 package day01;
-
-public class HelloWorld {
+/* 
+ * 여러줄 주석
+ */
+/** 
+ * 메소드를 설명할 때 사용하는 주석
+ * @param args
+ */
+public class HelloWorld { // keyword : 기능이 있는 단어
 
 	public static void main(String[] args) {
-
+		//sysout은 콘솔 출력할 때 사용
 		System.out.println("Hello World!!");
 		
 	}
