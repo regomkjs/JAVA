6b1f41b656559ed76ca1accaf221d589485aec15
regomkjs <regomkjs2013@gmail.com>
2023-12-12 17:39:02 +0900

    자바 3일차 추가 자습

--- java_kjs/src/self/SelfTestDay03.java
+++ java_kjs/src/self/SelfTestDay03.java
@@ -1,11 +1,15 @@
 package self;
 
+import java.util.Scanner;
+
 public class SelfTestDay03 {
 	
 	/* 자습용 자료
 	 * 3일차 
 	 * */
 	public static void main(String[] args) {
+		/*
+		
 		// 2단부터 9단까지 출력
 		int dan = 2;
 		
@@ -46,9 +50,58 @@ public class SelfTestDay03 {
 		System.out.println("==============================");
 		System.out.println('\n');
 		
+		// 1월부터 12월까지 각 월과 계절을 함께 출력하시오
+		
+		int month;
+		String ch;
+		
+		for (month = 1 ; month <= 12 ; month++ ) {
+			if (month < 3 || month == 12) {
+				ch = "겨울";
+				System.out.print(month +"월 " + ch + (month == 12 ? "\n" : ", "));
+			}
+			else if (month < 6) {
+				ch = "봄";
+				System.out.print(month +"월 " + ch + ", ");
+			}
+			else if (month < 9) {
+				ch = "여름";
+				System.out.print(month +"월 " + ch + ", ");
+			}
+			else {
+				ch = "가을";
+				System.out.print(month +"월 " + ch + ", ");
+			}
+		}
+		
+		*/
+		
+		/* 업 다운 복습 */
+		int min = 1;
+		int max = 100;
+		int count = 1;
+		int r = (int)(Math.random() * (max - min + 1) + min);
+		Scanner scan = new Scanner(System.in);
+		int num = 0;
+		for ( ; num != r ; ) {
+			System.out.print("정답 입력 : ");
+			num = scan.nextInt();
+			if (num < r) {
+				System.out.println("Up!");
+				count++;
+			}
+			else if (num > r) {
+				System.out.println("Down!");
+				count++;
+			}
+			else {
+				System.out.println("시도 : " + count);
+				System.out.println("정답 : " + r);
+			}
+		}
 		
 		
-		
+		scan.close();
 		
 		
 		
