8bb326af88d07c3c2fa417bd595417293041f20d
regomkjs <regomkjs2013@gmail.com>
2023-12-21 17:37:06 +0900

    자바 10일차 자습

---
+++ java_kjs/src/self/SelfTestDay10.java
@@ -0,0 +1,14 @@
+package self;
+
+public class SelfTestDay10 {
+
+	/* 1. 단어장을 만들되 수정에서 뜻을 추가할 수 있게 만들어라
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
