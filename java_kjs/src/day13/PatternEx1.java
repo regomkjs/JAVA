a55406de3d900d5f10243a6e6edc16c7b7a94bed
regomkjs <regomkjs2013@gmail.com>
2023-12-28 17:42:32 +0900

    자바 14일차 전날 주석 수정

--- java_kjs/src/day13/PatternEx1.java
+++ java_kjs/src/day13/PatternEx1.java
@@ -8,7 +8,7 @@ public class PatternEx1 {
 	
 	
 	public static void main(String[] args) {
-		// Pattern 클래스를 이용하여 문자열이 정규표현식에 맞는지 확인하는 예제
+		// Pattern 클래스를 이용하여 문자열이 정규표현식(Regex)에 맞는지 확인하는 예제
 		String str = "abc";
 		String regex = "^[a-zA-Z]{3}$";
 		if(Pattern.matches(regex, str)) {
