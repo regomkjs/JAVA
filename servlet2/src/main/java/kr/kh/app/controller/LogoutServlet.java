122b9f1a7791b4fff8d8b6b2171b7fbe4dd3f459
regomkjs <regomkjs2013@gmail.com>
2024-02-28 17:53:15 +0900

    Update LogoutServlet.java

--- servlet2/src/main/java/kr/kh/app/controller/LogoutServlet.java
+++ servlet2/src/main/java/kr/kh/app/controller/LogoutServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.kh.app.model.vo.MemberVO;
-
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
