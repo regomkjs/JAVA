8fb9ad6b4cad21e58b86d4c75da149b91f6a7013
regomkjs <regomkjs2013@gmail.com>
2024-02-29 15:51:09 +0900

    서블릿 6일차 복습(맴버필터, 게스트필터)

--- servlet/servlet_project1/src/main/java/kr/kh/app/filter/GuestFilter.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/filter/GuestFilter.java
@@ -15,17 +15,17 @@ import javax.servlet.http.HttpServletRequest;
 
 import kr.kh.app.model.vo.MemberVO;
 
-@WebFilter({"/board/insert","/board/update","/board/delete"})
-public class MemberFilter extends HttpFilter implements Filter {
+@WebFilter({"/login","/signup"})
+public class GuestFilter extends HttpFilter implements Filter {
 	private static final long serialVersionUID = 1L;
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		//회원 정보를 가져옴
 		MemberVO user = (MemberVO)((HttpServletRequest) request).getSession().getAttribute("user");
 		//회원 정보가 없으면 알림 후 로그인 페이지로
-		if(user == null) {
-			request.setAttribute("msg", "로그인이 필요한 서비스 입니다.");
-			request.setAttribute("url", "login");
+		if(user != null) {
+			request.setAttribute("msg", "로그인한 회원이 이용할 수 없는 서비스 입니다.");
+			request.setAttribute("url", "");
 			request.getRequestDispatcher("/WEB-INF/views/message.jsp").forward(request, response);
 			return;
 		}
