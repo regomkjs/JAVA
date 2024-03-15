dec9b82512c5d4b949e43e48d54961fc78c3f197
regomkjs <regomkjs2013@gmail.com>
2024-03-15 10:08:21 +0900

    스프링 3일차 : 게시글 페이지네이션 구현(검색X)

--- spring/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
+++ spring/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
@@ -23,5 +23,13 @@ public class BoardServiceImp implements BoardService {
 		return boardDao.selectBoardList(cri);
 		
 	}
+
+	@Override
+	public int getBoardTotalCount(Criteria cri) {
+		if(cri == null) {
+			cri = new Criteria(1, 5);
+		}
+		return boardDao.selectBoardTotalCount(cri);
+	}
 		
 }
